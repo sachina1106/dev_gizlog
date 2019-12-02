4ea98a8efa29f9cafa238e4081b7fc03d6be1d7a
sachina1106 <sachina.maki@gmail.com>
2019-12-02 13:47:24 +0900

    インデント修正

--- resources/views/user/dailyReport/create.blade.php
+++ resources/views/user/dailyReport/create.blade.php
@@ -5,17 +5,17 @@
 <div class="main-wrap">
   <div class="container">
     {!!Form::open(['route'=>'reports.store'])!!}
-      <div class="form-group form-size-small  {{ $errors->has('reporting_time')? 'has-error' : '' }}">
-{!!Form::input('date','reporting_time',null,['class'=>'form-control'])!!}
+    <div class="form-group form-size-small  {{ $errors->has('reporting_time') ? 'has-error' : '' }}">
+      {!!Form::input('date','reporting_time',null,['class'=>'form-control'])!!}
 
-    <span class="help-block">{{ $errors->first('reporting_time') }}</span>
+      <span class="help-block">{{ $errors->first('reporting_time') }}</span>
     </div>
-    <div class="form-group  {{ $errors->has('title')? 'has-error' : '' }}">
+    <div class="form-group  {{ $errors->has('title') ? 'has-error' : '' }}">
       {!!Form::input('text','title',null,['class'=>'form-control','placeholder'=>'Title'])!!}
       <span class="help-block">{{ $errors->first('title') }}</span>
     </div>
-    <div class="form-group  {{ $errors->has('content')? 'has-error' : '' }}">
-            {!!Form::textarea('content',null,['class'=>'form-control','placeholder'=>'Content'])!!}
+    <div class="form-group  {{ $errors->has('content') ? 'has-error' : '' }}">
+      {!!Form::textarea('content',null,['class'=>'form-control','placeholder'=>'Content'])!!}
       <span class="help-block">{{ $errors->first('content') }}</span>
     </div>
     {!!Form::submit('Add',['class'=>'btn btn-success pull-right'])!!}
