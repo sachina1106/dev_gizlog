39d3c1bc8152077de29085cb8788ebb794d85e5f
sachina1106 <sachina.maki@gmail.com>
2019-12-02 13:32:07 +0900

    seeder作成

---
+++ database/seeds/DailyReportsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class DailyReportsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        DB::table('daily_reports')->insert([
+            [
+            'reporting_time' => Carbon::create(11, 6),
+            'title' => 'test',
+            'user_id' => '1',
+            'content' => 'テスト',
+            ],
+            [
+                'reporting_time' => Carbon::create(11, 7),
+                'title' => 'test1',
+                'user_id' => '1',
+                'content' => 'テスト1',
+                ],
+        ]);
+    }
+}
