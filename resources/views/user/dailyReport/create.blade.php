07cd9bdf2c0def035499d9f3b8e5d2f730662ba9
sachina1106 <sachina.maki@gmail.com>
2019-12-03 15:19:15 +0900

    エラー時以外spanタグ非表示

--- resources/views/user/dailyReport/create.blade.php
+++ resources/views/user/dailyReport/create.blade.php
@@ -8,15 +8,15 @@
     <div class="form-group form-size-small  {{ $errors->has('reporting_time') ? 'has-error' : '' }}">
       {!!Form::input('date','reporting_time',null,['class'=>'form-control'])!!}
 
-      <span class="help-block">{{ $errors->first('reporting_time') }}</span>
+      @if($errors->has('reporting_time'))<span class="help-block">{{ $errors->first('reporting_time') }}</span>@endif
     </div>
     <div class="form-group  {{ $errors->has('title') ? 'has-error' : '' }}">
       {!!Form::input('text','title',null,['class'=>'form-control','placeholder'=>'Title'])!!}
-      <span class="help-block">{{ $errors->first('title') }}</span>
+      @if($errors->has('title'))<span class="help-block">{{ $errors->first('title') }}</span>@endif
     </div>
     <div class="form-group  {{ $errors->has('content') ? 'has-error' : '' }}">
       {!!Form::textarea('content',null,['class'=>'form-control','placeholder'=>'Content'])!!}
-      <span class="help-block">{{ $errors->first('content') }}</span>
+      @if($errors->has('content'))<span class="help-block">{{ $errors->first('content') }}</span>@endif
     </div>
     {!!Form::submit('Add',['class'=>'btn btn-success pull-right'])!!}
     {!!Form::close()!!}
