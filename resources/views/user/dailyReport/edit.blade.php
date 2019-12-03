f2cc2e5fd979c580612a12401957a33ed431fd6d
sachina1106 <sachina.maki@gmail.com>
2019-12-03 15:53:55 +0900

    エラーじゃないときspanタグ非表示

--- resources/views/user/dailyReport/edit.blade.php
+++ resources/views/user/dailyReport/edit.blade.php
@@ -7,15 +7,15 @@
     {!! Form::open(['route'=>['reports.update', $report->id], 'method'=>'PUT']) !!}
     <div class="form-group form-size-small {{ $errors->has('reporting_time') ? 'has-error' : '' }}">
       {!! Form::input('data', 'reporting_time', $report->reporting_time->format('Y-m-d'), ['class'=>'form-control'])!!}
-      <span class="help-block">{{ $errors->first('reporting_time') }}</span>
+      @if($errors->has('reporting_time'))<span class="help-block">{{ $errors->first('reporting_time') }}</span>@endif
     </div>
     <div class="form-group  {{ $errors->has('title') ? 'has-error' : '' }}">
       {!!Form::input('text', 'title', $report->title, ['class'=>'form-control'])!!}
-      <span class="help-block">{{ $errors->first('title') }}</span>
+      @if($errors->has('title'))<span class="help-block">{{ $errors->first('title') }}</span>@endif
     </div>
     <div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
       {!!Form::textarea('content', $report->content, ['class'=>'form-control'])!!}
-      <span class="help-block">{{ $errors->first('content') }}</span>
+      @if($errors->has('content'))<span class="help-block">{{ $errors->first('content') }}</span>@endif
     </div>
     {!!Form::submit('Update', ['class'=>'btn btn-success pull-right'])!!}
     {!!Form::close()!!}
