c21bbf38e21a07a4c1815efcdd91711d595620a8
sachina1106 <sachina.maki@gmail.com>
2019-12-06 15:57:23 +0900

    インデント修正,model関数修正

--- app/Models/DailyReport.php
+++ app/Models/DailyReport.php
@@ -20,12 +20,17 @@ class DailyReport extends Model
         'reporting_time',
     ];
 
+    public function fetchReportByUserId($id)
+    {
+        return $this->where('user_id', $id);
+    }
+
     public function fetchDailyReportByUserId($id, $searchMonth)
     {
-        if (empty($searchMonth)) {
-            return  $reports = $this->where('user_id', $id)->paginate(10);
-        } else {
-            return  $reports = $this->where('user_id', $id)->where('reporting_time', 'like', $searchMonth.'%')->paginate(10);
+        if (!empty($searchMonth)) {
+            return  $this->fetchReportByUserId($id)->where('reporting_time', 'like', $searchMonth.'%')->paginate(10);
         }
+
+        return  $this->fetchReportByUserId($id)->paginate(10);
     }
 }
